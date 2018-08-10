d741ac7d7652bd320fb26110aae6b5237eb1c3cc
yanming <arist1213@163.com>
2018-08-10 17:03:33 +0800

    +websocket demo

---
+++ websocket-demo/src/main/java/com/example/websocketdemo/HelloMessage.java
@@ -0,0 +1,24 @@
+package com.example.websocketdemo;
+
+/**
+ * created by yanming on 2018/8/10
+ */
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
